5a04e7caac7ac780d4e0364f73a69525e5199daa
Michael Goodnow <mmgoodnow@gmail.com>
2020-06-08 16:21:05 -0400

    Filter by number of files and file extensions

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 #!/usr/bin/env node
 
-import parseTorrent from "parse-torrent";
+const parseTorrent = require("parse-torrent");
+const fs = require("fs");
+const path = require("path");
 
-console.log(parseTorrent);
+function parseTorrentFromFilename(filename) {
+	const data = fs.readFileSync(filename);
+	const torrentInfo = parseTorrent(data);
+	return torrentInfo;
+}
+
+function filterTorrentFile(info) {
+	if (info.files.length < 4) {
+		console.log(`not enough files: ${info.name}`);
+		return false;
+	}
+	const allMkvs = info.files.every(file => file.path.endsWith(".mkv"));
+	if (!allMkvs) {
+		console.log(`not all mkvs: ${info.name}`);
+		return false;
+	}
+	return true;
+}
+
+function main() {
+	const dirContents = fs.readdirSync("torrents")
+		.map(fn => path.join("torrents", fn));
+	const parsedTorrents = dirContents.map(parseTorrentFromFilename);
+	const filteredTorrents = parsedTorrents.filter(filterTorrentFile);
+	console.log(filteredTorrents.map(x => x.name));
+}
+
+main();
