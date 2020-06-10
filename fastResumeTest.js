8d7cd034f2383967ba3f7e4998b29b1626e5a9b3
Michael Goodnow <mmgoodnow@gmail.com>
2020-06-10 02:19:10 -0400

    Remove fastResume stuff

--- fastResumeTest.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const fs = require("fs");
-const { encode, decode } = require("bencode");
-const resume = require("./fastResume");
-const datapath = process.argv[2];
-const data = fs.readFileSync("/dev/stdin");
-const withResumeEncoded = encode(resume(decode(data), datapath));
-fs.writeFileSync("/dev/stdout", withResumeEncoded);
