b51385460c341c8505a9992cb1ae9cd707b3805e
Michael Goodnow <mmgoodnow@gmail.com>
2020-10-11 19:53:34 -0400

    Add logger file

---
+++ src/logger.js
@@ -0,0 +1,22 @@
+const { getRuntimeConfig } = require("./configuration");
+
+function log(...args) {
+	console.log(...args);
+}
+
+function verbose(...args) {
+	const { verbose } = getRuntimeConfig();
+	if (verbose) {
+		console.log(...args);
+	}
+}
+
+function error(...args) {
+	console.error(...args);
+}
+
+function warn(...args) {
+	console.warn(...args);
+}
+
+module.exports = { log, verbose, error, warn };
