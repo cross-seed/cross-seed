7c4985b75940b032575bcd05eb80f0e5c913d77f
Michael Goodnow <mmgoodnow@gmail.com>
2021-03-07 01:14:59 -0500

    Switch to ESM on constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,13 @@
-exports.EP_REGEX = /^(?<title>.+)[. ](?<season>S\d+)(?<episode>E\d+)/i;
-exports.SEASON_REGEX = /^(?<title>.+)[. ](?<season>S\d+)(?:\s?-\s?(?<seasonmax>S?\d+))?(?!E\d+)/i;
-exports.MOVIE_REGEX = /^(?<title>.+)[. ][[(]?(?<year>\d{4})[)\]]?(?![pi])/i;
+export const EP_REGEX = /^(?<title>.+)[. ](?<season>S\d+)(?<episode>E\d+)/i;
+export const SEASON_REGEX = /^(?<title>.+)[. ](?<season>S\d+)(?:\s?-\s?(?<seasonmax>S?\d+))?(?!E\d+)/i;
+export const MOVIE_REGEX = /^(?<title>.+)[. ][[(]?(?<year>\d{4})[)\]]?(?![pi])/i;
 
-exports.EXTENSIONS = ["mkv", "mp4", "avi"];
+export const EXTENSIONS = ["mkv", "mp4", "avi"];
 
-exports.CONFIG_TEMPLATE_URL =
+export const CONFIG_TEMPLATE_URL =
 	"https://github.com/mmgoodnow/cross-seed/blob/master/src/config.template.js";
 
-exports.ACTIONS = {
+export const ACTIONS = {
 	SAVE: "save",
 	INJECT: "inject",
 };
@@ -22,7 +22,7 @@ const _result = {
 	Guid: undefined,
 };
 
-exports.InjectionResult = {
+export const InjectionResult = {
 	SUCCESS: 1,
 	FAILURE: -1,
 	ALREADY_EXISTS: 0,
