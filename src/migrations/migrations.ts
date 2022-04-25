1759079be14b2e0c58b4bb75e349fdbd34f12f48
Michael Goodnow <mmgoodnow@gmail.com>
2022-04-25 14:42:30 -0400

    Add note about migrations

--- src/migrations/migrations.ts
+++ src/migrations/migrations.ts
@@ -1,5 +1,7 @@
 import initialSchema from "./00-initialSchema.js";
 
+// The first step of any migration should be to back up the database.
+
 export const migrations = {
 	getMigrations: () => Promise.resolve([initialSchema]),
 	getMigrationName: (migration) => migration.name,
