837223d7802739133f7a1c62f89690c4d8343dc8
Michael Goodnow <mmgoodnow@gmail.com>
2022-05-20 22:47:15 -0400

    Remove broken links to blog

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -69,7 +69,6 @@ const config = {
 						position: "left",
 						label: "Tutorial",
 					},
-					{ to: "/blog", label: "Blog", position: "left" },
 					{
 						href: "https://github.com/facebook/docusaurus",
 						label: "GitHub",
