7f63c934cdd333b7536ace135d0480ccbfe1ae2c
Michael Goodnow <mmgoodnow@gmail.com>
2023-02-05 04:10:00 -0500

    Move WAL pragma out of migration

--- src/migrations/migrations.ts
+++ src/migrations/migrations.ts
@@ -2,11 +2,10 @@ import initialSchema from "./00-initialSchema.js";
 import jobs from "./01-jobs.js";
 import timestamps from "./02-timestamps.js";
 import rateLimits from "./03-rateLimits.js";
-import wal from "./04-wal.js";
 
 export const migrations = {
 	getMigrations: () =>
-		Promise.resolve([initialSchema, jobs, timestamps, rateLimits, wal]),
+		Promise.resolve([initialSchema, jobs, timestamps, rateLimits]),
 	getMigrationName: (migration) => migration.name,
 	getMigration: (migration) => migration,
 };
