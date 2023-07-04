ef60d7a72218cdb8636c517fc372e76f8feb75d9
Michael Goodnow <mmgoodnow@gmail.com>
2023-07-04 17:15:49 -0400

    uninstall parse-torrent

--- src/index.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-unused-vars */
-
-declare module "parse-torrent" {
-	export = ParseTorrent;
-	function ParseTorrent(torrentId: Buffer): ParseTorrent.Metafile;
-	namespace ParseTorrent {}
-}
