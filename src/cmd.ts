220247179d3ce0af5d1c164758c200035d302f80
Michael Goodnow <mmgoodnow@gmail.com>
2023-07-07 16:40:53 -0400

    add to clear-cache

--- src/cmd.ts
+++ src/cmd.ts
@@ -241,6 +241,13 @@ program
 	.description("Clear the cache of downloaded-and-rejected torrents")
 	.action(async () => {
 		await db("decision").del();
+		await db("indexer").update({
+			status: null,
+			retry_after: null,
+			search_cap: null,
+			tv_search_cap: null,
+			movie_search_cap: null,
+		});
 		await db.destroy();
 	});
 
