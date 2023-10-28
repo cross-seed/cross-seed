a94ee19a0af9fa56c110b187d49ee32c761cffd9
zakary <123845855+zakkarry@users.noreply.github.com>
2023-10-28 10:44:19 -0500

    initial(zod): adds zod schema validation for config.js
    Initial Zod schemas for validating settings in config.js

--- src/startup.ts
+++ src/startup.ts
@@ -1,11 +1,13 @@
 import { getClient } from "./clients/TorrentClient.js";
-import { MatchMode } from "./constants.js";
+import { getFileConfig } from "./configuration.js";
+import { MatchMode, Action } from "./constants.js";
 import { CrossSeedError } from "./errors.js";
 import { logger } from "./logger.js";
 import { getRuntimeConfig } from "./runtimeConfig.js";
-import { validateTorrentDir } from "./torrent.js";
 import { validateTorznabUrls } from "./torznab.js";
-import { validateAction } from "./action.js";
+import { VALIDATION_SCHEMA } from "./zod.js";
+
+const fileConfig = await getFileConfig();
 
 function validateOptions() {
 	const {
@@ -43,18 +45,113 @@ function validateOptions() {
 		);
 	}
 }
+export async function validateWithZod() {
+	const {
+		action,
+		rtorrentRpcUrl,
+		qbittorrentUrl,
+		transmissionRpcUrl,
+		dataDirs,
+		outputDir,
+		torrentDir,
+		dataCategory,
+		linkDir,
+		notificationWebhookUrl,
+		linkType,
+		matchMode,
+		skipRecheck,
+		fuzzySizeThreshold,
+		port,
+		excludeOlder,
+		excludeRecentSearch,
+		rssCadence,
+		searchCadence,
+		searchLimit,
+		includeSingleEpisodes,
+		includeEpisodes,
+		includeNonVideos,
+		duplicateCategories,
+	} = getRuntimeConfig();
+	try {
+		VALIDATION_SCHEMA.ACTION.parse(action);
+		if (action === Action.INJECT) {
+			if (qbittorrentUrl) {
+				VALIDATION_SCHEMA.URL.parse(qbittorrentUrl);
+			} else if (rtorrentRpcUrl) {
+				VALIDATION_SCHEMA.URL.parse(rtorrentRpcUrl);
+			} else if (transmissionRpcUrl) {
+				VALIDATION_SCHEMA.URL.parse(transmissionRpcUrl);
+			}
+		}
 
+		const downloadClient = getClient();
+		downloadClient?.validateConfig();
+		validateTorznabUrls();
+
+		if (notificationWebhookUrl) {
+			VALIDATION_SCHEMA.URL.parse(notificationWebhookUrl);
+		}
+
+		if (excludeOlder) {
+			VALIDATION_SCHEMA.MS.parse(excludeOlder);
+		} else if (!excludeOlder && fileConfig.excludeOlder) {
+			throw new CrossSeedError(
+				"your interval does not comply with vercel's 'ms' format"
+			);
+		}
+
+		if (excludeRecentSearch) {
+			VALIDATION_SCHEMA.MS.parse(excludeRecentSearch);
+		} else if (!excludeRecentSearch && fileConfig.excludeRecentSearch) {
+			throw new CrossSeedError(
+				"your interval does not comply with vercel's 'ms' format"
+			);
+		}
+		if (rssCadence) {
+			VALIDATION_SCHEMA.MS.parse(rssCadence);
+		} else if (!rssCadence && fileConfig.rssCadence) {
+			throw new CrossSeedError(
+				"your interval does not comply with vercel's 'ms' format"
+			);
+		}
+		if (searchCadence) {
+			VALIDATION_SCHEMA.MS.parse(searchCadence);
+		} else if (!searchCadence && fileConfig.searchCadence) {
+			throw new CrossSeedError(
+				"your interval does not comply with vercel's 'ms' format"
+			);
+		}
+
+		VALIDATION_SCHEMA.STRING.parse(dataCategory);
+		VALIDATION_SCHEMA.FUZZY.parse(fuzzySizeThreshold);
+		VALIDATION_SCHEMA.PORT.parse(port);
+		VALIDATION_SCHEMA.MATCHMODE.parse(matchMode);
+		if (searchLimit) {
+			VALIDATION_SCHEMA.NUMBER.parse(searchLimit);
+		}
+
+		VALIDATION_SCHEMA.BOOLEAN.parse(skipRecheck);
+		VALIDATION_SCHEMA.BOOLEAN.parse(includeEpisodes);
+		VALIDATION_SCHEMA.BOOLEAN.parse(includeSingleEpisodes);
+		VALIDATION_SCHEMA.BOOLEAN.parse(includeNonVideos);
+		VALIDATION_SCHEMA.BOOLEAN.parse(duplicateCategories);
+
+		if (dataDirs) {
+			await VALIDATION_SCHEMA.PATH.parseAsync(dataDirs);
+			await VALIDATION_SCHEMA.PATH.parseAsync(linkDir);
+			await VALIDATION_SCHEMA.LINKTYPE.parseAsync(linkType);
+		}
+		await VALIDATION_SCHEMA.PATH.parseAsync(outputDir);
+		await VALIDATION_SCHEMA.PATH.parseAsync(torrentDir);
+	} catch (errors) {
+		throw new CrossSeedError(errors);
+	}
+
+	logger.info("Your configuration is zod valid!");
+}
 export async function doStartupValidation(): Promise<void> {
 	logger.info("Validating your configuration...");
 	validateOptions();
-	const downloadClient = getClient();
-	await Promise.all<void>(
-		[
-			validateAction(),
-			validateTorznabUrls(),
-			downloadClient?.validateConfig(),
-			validateTorrentDir(),
-		].filter(Boolean)
-	);
+	await Promise.all<void>([validateWithZod()]);
 	logger.info("Your configuration is valid!");
 }
