609e3d4a8ba86b1087004d32c5b0e1e845327495
zakary <123845855+zakkarry@users.noreply.github.com>
2023-10-28 21:24:30 -0500

    update(config/zod): using zod object to validate getRuntimeConfig()
    change approach to unify the zod schemas into a single z.object and validate against getRuntimeConfig()
    
    removed validateOptions and consolidated into VALIDATION_SCHEMA
    
    Co-Authored-By: Colin Hebert <makkhdyn@gmail.com>

--- src/zod.ts
+++ src/zod.ts
@@ -1,231 +1,211 @@
-/**
- * Zod validation schemas for config.js.
- *
- * All validate functions accept value for configs setting
- */
-
 import { z } from "zod";
 import { Action, LinkType, MatchMode } from "./constants.js";
 import { existsSync } from "fs";
+import { logger } from "./logger.js";
+import { getFileConfig } from "./configuration.js";
+
+const fileConfig = await getFileConfig();
 
 /**
  * VALIDATION_SCHEMA is just a object of all the zod schemas
  * each are named after what they are intended to validate
  */
-export const VALIDATION_SCHEMA = {
-	URL: z.union([z.string(), z.array(z.string())]).refine(
-		(values) => {
-			const validateUrl = (url) => {
-				try {
-					new URL(url);
-					return true;
-				} catch (error) {
-					return false;
-				}
-			};
 
-			if (Array.isArray(values)) {
-				return values.every(
-					(url) => typeof url === "string" && validateUrl(url)
-				);
-			} else {
-				return typeof values === "string" && validateUrl(values);
-			}
-		},
-		{
-			message: "invalid URL",
-		}
-	),
-	PATH: z.union([z.string(), z.array(z.string())]).refine(
-		(values) => {
-			const validatePath = (value) => {
-				if (typeof value !== "string" || !existsSync(value)) {
-					return false;
-				}
-				return true;
-			};
-
-			if (Array.isArray(values)) {
-				return values.every(validatePath);
-			} else {
-				return validatePath(values);
-			}
-		},
-		{
-			message: "one or more paths do not exist on the filesystem",
-		}
-	),
-	BOOLEAN: z
-		.boolean()
-		.refine((value) => value !== undefined && value !== null, {
-			message: "value must be a defined boolean (true or false)",
-		}),
-	PORT: z.number().refine((value) => value >= 1 && value <= 65535, {
-		message: "port must be a number between 1 and 65535",
-	}),
-	NUMBER: z.union([z.number(), z.undefined()]).refine(
-		(value) => {
-			return typeof value === "number" || value === undefined;
-		},
-		{
-			message: "value must be a number or undefined",
-		}
-	),
-	DELAY: z.number().refine((value) => value >= 1 && value <= 1000, {
-		message:
-			"delay is in seconds, >1000 implies you think it's milliseconds",
-	}),
-	ACTION: z
-		.string()
-		.refine((value) => value === Action.SAVE || value === Action.INJECT, {
-			message: `value must be either '${Action.SAVE}' or '${Action.INJECT}'`,
-		}),
-	MATCHMODE: z
-		.enum([MatchMode.SAFE, MatchMode.RISKY])
-		.refine(
-			(value) => value === MatchMode.RISKY || value === MatchMode.SAFE,
-			{
-				message: `value must be either '${MatchMode.RISKY}' or '${MatchMode.SAFE}'`,
-			}
+export const VALIDATION_SCHEMA = z
+	.object({
+		notificationWebhookUrl: z.union([
+			z.undefined(),
+			z
+				.string()
+				.url()
+				.refine((url) => ({
+					message: `invalid notificationWebhookUrl URL - ${url}`,
+				})),
+		]),
+		torznab: z.array(
+			z
+				.string()
+				.url()
+				.refine((url) => ({
+					message: `invalid torznab URL - ${url}`,
+				}))
 		),
-	LINKTYPE: z
-		.enum([LinkType.SYMLINK, LinkType.HARDLINK])
-		.refine(
-			(value) =>
-				value === LinkType.HARDLINK || value === LinkType.SYMLINK,
-			{
-				message: `value must be either '${LinkType.HARDLINK}' or '${LinkType.SYMLINK}'`,
-			}
+		dataDirs: z.union([
+			z.undefined(),
+			z.array(
+				z.string().refine(
+					(path) => existsSync(path),
+					(path) => ({
+						message: `the dataDirs path ${path} does not exist on the filesystem`,
+					})
+				)
+			),
+		]),
+		torrentDir: z.string().refine(
+			(path) => existsSync(path),
+			(path) => ({
+				message: `the torrentDir path ${path} does not exist on the filesystem`,
+			})
 		),
-	STRING: z
-		.string()
-		.min(1, { message: "value must have at least 1 character" })
-		.max(24, { message: "value can have a max of 24 characters" }),
-	FUZZY: z.number().refine((value) => value > 0 && value < 1, {
-		message: "must be a decimal percentage greater than 0 and less than 1",
-	}),
-	MS: z.number().refine(
-		(value) => {
-			return value !== undefined;
-		},
-		{
+		linkDir: z.union([
+			z.undefined(),
+			z.string().refine(
+				(path) => existsSync(path),
+				(path) => ({
+					message: `the linkDir path ${path} does not exist on the filesystem`,
+				})
+			),
+		]),
+		outputDir: z.string().refine(
+			(path) => existsSync(path),
+			(path) => ({
+				message: `the outputDir path ${path} does not exist on the filesystem`,
+			})
+		),
+		includeEpisodes: z.boolean({
+			invalid_type_error:
+				"includeEpisodes must be a defined boolean (true or false)",
+		}),
+		includeSingleEpisodes: z.boolean({
+			invalid_type_error:
+				"includeSingleEpisodes must be a defined boolean (true or false)",
+		}),
+		includeNonVideos: z.boolean({
+			invalid_type_error:
+				"includeNonVideos must be a defined boolean (true or false)",
+		}),
+		duplicateCategories: z.boolean({
+			invalid_type_error:
+				"duplicateCategories must be a defined boolean (true or false)",
+		}),
+		skipRecheck: z.boolean({
+			invalid_type_error:
+				"skipRecheck must be a defined boolean (true or false)",
+		}),
+		maxDataDepth: z.number().gte(1, {
+			message: "maxDataDepth must be a number greater than 0",
+		}),
+		port: z.number().gte(1).lte(65535, {
+			message: "port must be a number between 1 and 65535",
+		}),
+		searchLimit: z.union([z.number(), z.undefined()], {
+			invalid_type_error: "searchLimit must be a number or undefined",
+		}),
+		delay: z.number().gt(1).lt(1000, {
 			message:
-				"value must be in Vercel's 'ms' time format (e.g., '1d 2h 3m 4s')",
+				"delay is in seconds, >1000 implies you think it's milliseconds",
+		}),
+		action: z.nativeEnum(Action, {
+			invalid_type_error: `action must be either '${Action.SAVE}' or '${Action.INJECT}'`,
+		}),
+		qbittorrentUrl: z.string().url().optional(),
+		rtorrentRpcUrl: z.string().url().optional(),
+		transmissionRpcUrl: z.string().url().optional(),
+
+		matchMode: z.nativeEnum(MatchMode, {
+			invalid_type_error: `matchMode must be either '${MatchMode.RISKY}' or '${MatchMode.SAFE}'`,
+		}),
+		linkType: z.nativeEnum(LinkType, {
+			invalid_type_error: `linkType must have a value of "${LinkType.HARDLINK}" or "${LinkType.SYMLINK}"`,
+		}),
+		dataCategory: z.union([
+			z.undefined(),
+			z.string().min(1).max(24, {
+				message: "dataCategory must have a length of 1-24 characters",
+			}),
+		]),
+		fuzzySizeThreshold: z.number().gt(0).lt(1, {
+			message:
+				"fuzzySizeThreshold must be a decimal percentage greater than 0 and less than 1",
+		}),
+		searchCadence: z.union([
+			z.number(),
+			z.undefined(),
+			z.nan().refine(
+				() => {
+					return typeof fileConfig.searchCadence === "undefined";
+				},
+				{
+					message:
+						"your searchCadence does not follow vercel's `ms` style",
+				}
+			),
+		]),
+		rssCadence: z.union([
+			z.number(),
+			z.undefined(),
+			z.nan().refine(
+				() => {
+					return typeof fileConfig.rssCadence === "undefined";
+				},
+				{
+					message:
+						"your rssCadence does not follow vercel's `ms` style",
+				}
+			),
+		]),
+		excludeOlder: z.union([
+			z.number(),
+			z.undefined(),
+			z.nan().refine(
+				() => {
+					return typeof fileConfig.excludeOlder === "undefined";
+				},
+				{
+					message:
+						"your excludeOlder does not follow vercel's `ms` style",
+				}
+			),
+		]),
+		excludeRecentSearch: z.union([
+			z.number(),
+			z.undefined(),
+			z.nan().refine(
+				() => {
+					return (
+						typeof fileConfig.excludeRecentSearch === "undefined"
+					);
+				},
+				{
+					message:
+						"your excludeRecentSearch does not follow vercel's `ms` style",
+				}
+			),
+		]),
+	})
+	.refine(
+		(config) => {
+			if (config.action === Action.INJECT) {
+				return (
+					config.rtorrentRpcUrl ||
+					config.qbittorrentUrl ||
+					config.transmissionRpcUrl
+				);
+			}
+			return true;
+		},
+		() => ({
+			message:
+				"You need to specify rtorrentRpcUrl, transmissionRpcUrl, or qbittorrentUrl when using 'inject'.",
+		})
+	)
+	.refine(
+		(config) => {
+			if (config.dataDirs || config.linkDir) {
+				return config.dataDirs && config.linkDir && config.linkType;
+			}
+			return true;
+		},
+		() => ({
+			message:
+				"Data-Based Matching requires linkType, dataDirs, and linkDir to be defined.",
+		})
+	)
+	.refine((config) => {
+		if (config.skipRecheck && config.matchMode == MatchMode.RISKY) {
+			logger.warn(
+				"It is strongly recommended to not skip rechecking for risky matching mode"
+			);
 		}
-	),
-};
-
-/**
- * validation for URL(s) in a string or array
- */
-export function validateUrls(value) {
-	try {
-		VALIDATION_SCHEMA.URL.parse(value);
-	} catch (error) {
-		console.log(error.message);
-	}
-}
-
-/**
- * validation for path's existance on the fs
- * accepts arrays or strings
- */
-
-export function validatePaths(value) {
-	try {
-		VALIDATION_SCHEMA.PATH.parse(value);
-	} catch (error) {
-		console.log(error.message);
-	}
-}
-
-/**
- *  Boolean validation.
- */
-export function validateBoolean(value) {
-	try {
-		VALIDATION_SCHEMA.BOOLEAN.parse(value);
-	} catch (error) {
-		console.log(error.message);
-	}
-}
-
-/**
- * Port (numeral) validation. 1-65535
- */
-export function validatePort(value) {
-	try {
-		VALIDATION_SCHEMA.PORT.parse(value);
-	} catch (error) {
-		console.log(error.message);
-	}
-}
-
-/**
- * Delay validation, assume >1000 means they think its milliseconds
- * Anything that high is stupid to have set as delay anyway.
- */
-export function validateDelay(value) {
-	try {
-		VALIDATION_SCHEMA.DELAY.parse(value);
-	} catch (error) {
-		console.log(error.message);
-	}
-}
-/**
- * action validation, either Action.SAVE or Action.INJECT
- */
-
-export function validateAction(value) {
-	try {
-		VALIDATION_SCHEMA.ACTION.parse(value);
-	} catch (error) {
-		console.log(error.message);
-	}
-}
-
-/**
- * matchMode validation, either MatchMode.RISKY or MatchMode.SAFE
- */
-
-export function validateMatchMode(value) {
-	try {
-		VALIDATION_SCHEMA.MATCHMODE.parse(value);
-	} catch (error) {
-		console.log(error.message);
-	}
-}
-
-/**
- * linkType validation, either LinkType.HARDLINK or LinkType.SYMLINK
- */
-export function validateLinkType(value) {
-	try {
-		VALIDATION_SCHEMA.LINKTYPE.parse(value);
-	} catch (error) {
-		console.log(error.message);
-	}
-}
-
-/**
- * Simple string validation, max 24 characters (dataCategory)
- */
-export function validateString(value) {
-	try {
-		VALIDATION_SCHEMA.STRING.parse(value);
-	} catch (error) {
-		console.log(error.message);
-	}
-}
-
-/**
- * fuzzyThreshold validation. Must be a percentage in decimal notation.
- */
-
-export function validateFuzzyThreshold(value) {
-	try {
-		VALIDATION_SCHEMA.FUZZY.parse(value);
-	} catch (error) {
-		console.log(error.message);
-	}
-}
+		return true;
+	});
