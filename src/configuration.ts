de493f7d8507d3ffc625b827f5312dd7f0438c0b
Michael Goodnow <mmgoodnow@gmail.com>
2024-03-14 01:45:12 -0400

    --wip-- [skip ci]

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,15 +1,16 @@
 import chalk from "chalk";
-import { copyFileSync, existsSync, mkdirSync } from "fs";
+import { accessSync, copyFileSync, existsSync, mkdirSync } from "fs";
 import { createRequire } from "module";
 import path from "path";
 import { pathToFileURL } from "url";
 import { Action, MatchMode } from "./constants.js";
+import { CrossSeedError } from "./errors.js";
 const require = createRequire(import.meta.url);
 const packageDotJson = require("../package.json");
 
-interface FileConfig {
+export interface FileConfig {
 	action?: Action;
-	configVersion?: number;
+	pconfigVersion?: number;
 	delay?: number;
 	includeEpisodes?: boolean;
 	includeSingleEpisodes?: boolean;
@@ -53,16 +54,17 @@ Make sure that
   - strings (words, URLs, etc) are wrapped in "quotation marks"
   - any arrays (lists of things, even one thing) are wrapped in [square brackets]
   - every entry has a comma after it, including inside arrays
-The location of the error is below, but you may have to look backwards to see where the root cause is. 
+The location of the error is above, but you may have to look backwards to see where the root cause is. 
 `.trim();
 
 export function appDir(): string {
-	return (
+	const appDir =
 		process.env.CONFIG_DIR ||
 		(process.platform === "win32"
 			? path.resolve(process.env.LOCALAPPDATA!, packageDotJson.name)
-			: path.resolve(process.env.HOME!, `.${packageDotJson.name}`))
-	);
+			: path.resolve(process.env.HOME!, `.${packageDotJson.name}`));
+	accessSync(appDir);
+	return appDir;
 }
 
 export function createAppDir(): void {
@@ -100,12 +102,10 @@ export async function getFileConfig(): Promise<FileConfig> {
 		if (e.code === "ERR_MODULE_NOT_FOUND") {
 			return {};
 		} else if (e instanceof SyntaxError) {
-			const prettyStack = e
-				.stack!.split("\n")
-				.filter((l) => !l.trim().startsWith("at"))
-				.join("\n");
-			const error = new Error(UNPARSABLE_CONFIG_MESSAGE);
-			error.stack = prettyStack;
+			const location = e.stack!.split("\n").slice(0, 3).join("\n");
+			const error = new CrossSeedError(
+				`${chalk.red(location)}\n\n${UNPARSABLE_CONFIG_MESSAGE}`
+			);
 			throw error;
 		} else {
 			throw e;
