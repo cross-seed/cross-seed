c3295315ab9cf25f34e6c3c540eb1f0435d47e51
Michael Goodnow <mmgoodnow@gmail.com>
2024-03-21 17:37:16 -0600

    Add logging for exception

--- src/clients/Deluge.ts
+++ src/clients/Deluge.ts
@@ -279,6 +279,7 @@ export default class Deluge implements TorrentClient {
 				return InjectionResult.FAILURE;
 			}
 		} catch (injectResult) {
+			console.error(injectResult);
 			if (injectResult.includes("label.set_torrent")) {
 				logger.warning({
 					label: Label.DELUGE,
