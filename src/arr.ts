2d9a2f289dde45231bacf5c55c376127318e8552
zakary <zak@ary.dev>
2024-04-18 19:12:57 -0500

    refactor(idlookup): another refactor and logging improvements

--- src/arr.ts
+++ src/arr.ts
@@ -3,6 +3,7 @@ import { getRuntimeConfig } from "./runtimeConfig.js";
 import { Result, resultOf, resultOfErr } from "./Result.js";
 import { MediaType } from "./utils.js";
 import { getApikey, sanitizeUrl } from "./torznab.js";
+import chalk from "chalk";
 export type ArrJson = {
 	movie?: idData;
 	series?: idData;
@@ -17,38 +18,33 @@ async function fetchArrJSON(
 	url: string,
 	mediaType: MediaType
 ): Promise<idData> {
-	try {
-		const uarrl = { apikey: getApikey(url), url: sanitizeUrl(url) };
-		const lookupUrl = `${uarrl.url}/v3/parse?apikey=${uarrl.apikey}&title=${searchterm}`;
-		const response = await fetch(lookupUrl);
-		let parsedData: idData;
-		if (!response.ok) {
-			logger.warn(
-				`unable to lookup corresponding id for ${searchterm}: Status Code -> ${response.status} `
-			);
-			throw new Error(`HTTP error! Status: ${response.status}`);
-		}
-		const arrJson = (await response.json()) as ArrJson;
-		if (mediaType === MediaType.EPISODE || mediaType === MediaType.SEASON) {
-			parsedData = {
-				imdbId: arrJson?.series?.imdbId,
-				tmdbId: arrJson?.series?.tmdbId,
-				tvdbId: arrJson?.series?.tvdbId,
-			};
-		} else if (mediaType === MediaType.MOVIE) {
-			parsedData = {
-				imdbId: arrJson?.movie?.imdbId,
-				tmdbId: arrJson?.movie?.tmdbId,
-				tvdbId: arrJson?.movie?.tvdbId,
-			};
-		} else {
-			parsedData = {};
-		}
-		return parsedData;
-	} catch (error) {
-		logger.error(`failed to lookup id for ${searchterm}: ${error}`);
-		throw error;
+	const uarrl = { apikey: getApikey(url), url: sanitizeUrl(url) };
+	const lookupUrl = `${uarrl.url}/v3/parse?apikey=${uarrl.apikey}&title=${searchterm}`;
+	const response = await fetch(lookupUrl);
+	let parsedData: idData;
+	if (!response.ok) {
+		logger.warn(
+			`unable to lookup corresponding id for ${searchterm}: Status Code -> ${response.status} `
+		);
+		throw new Error(`HTTP error! Status: ${response.status}`);
 	}
+	const arrJson = (await response.json()) as ArrJson;
+	if (mediaType === MediaType.EPISODE || mediaType === MediaType.SEASON) {
+		parsedData = {
+			imdbId: arrJson?.series?.imdbId,
+			tmdbId: arrJson?.series?.tmdbId,
+			tvdbId: arrJson?.series?.tvdbId,
+		};
+	} else if (mediaType === MediaType.MOVIE) {
+		parsedData = {
+			imdbId: arrJson?.movie?.imdbId,
+			tmdbId: arrJson?.movie?.tmdbId,
+			tvdbId: arrJson?.movie?.tvdbId,
+		};
+	} else {
+		parsedData = {};
+	}
+	return parsedData;
 }
 export async function grabArrId(
 	searchterm: string,
@@ -70,12 +66,35 @@ export async function grabArrId(
 				sonarrApi!,
 				mediaType
 			)) as idData;
-			logger.info(
-				`idLookup results -> TVDB: ${arrJson.tvdbId} IMDB: ${arrJson.imdbId} TMDB: ${arrJson.tmdbId}`
-			);
+			if (!isEmptyObject(arrJson)) {
+				logger.info(
+					`[sonarr-api] ${chalk.cyan(
+						"Found movie"
+					)} -> ${chalk.yellow("TVDB")}: ${chalk.white(
+						arrJson.tvdbId
+					)} ${chalk.yellow("IMDB")}: ${chalk.white(
+						arrJson.imdbId
+					)} ${chalk.yellow("TMDB")}: ${chalk.white(arrJson.tmdbId)}`
+				);
+			} else {
+				logger.error(
+					`[sonarr-api] Lookup for ${chalk.yellow(
+						searchterm
+					)} failed.`
+				);
+				logger.warn(
+					`[sonarr-api] Make sure the series is added to Sonarr.`
+				);
+			}
 			return resultOf(arrJson);
 		} catch (error) {
-			logger.error(`failed to lookup id for ${searchterm}: ${error}`);
+			logger.error(
+				`[sonarr-api] Failed to lookup IDs for ${chalk.yellow(
+					searchterm
+				)} - (${chalk.red(
+					String(error).split(":").slice(1)[0].trim()
+				)})`
+			);
 			return resultOfErr(false);
 		}
 	} else if (mediaType === MediaType.MOVIE) {
@@ -85,12 +104,33 @@ export async function grabArrId(
 				radarrApi!,
 				mediaType
 			)) as idData;
-			logger.info(
-				`idLookup results -> IMDB: ${arrJson.imdbId} TMDB: ${arrJson.tmdbId}`
-			);
+			if (!isEmptyObject(arrJson)) {
+				logger.info(
+					`[radarr-api] ${chalk.cyan(
+						"Found movie"
+					)} -> ${chalk.yellow("IMDB")}: ${chalk.white(
+						arrJson.imdbId
+					)} ${chalk.yellow("TMDB")}: ${chalk.white(arrJson.tmdbId)}`
+				);
+			} else {
+				logger.error(
+					`[radarr-api] Lookup for ${chalk.yellow(
+						searchterm
+					)} failed.`
+				);
+				logger.warn(
+					`[radarr-api] Make sure the movie is added to Radarr.`
+				);
+			}
 			return resultOf(arrJson);
 		} catch (error) {
-			logger.error(`failed to lookup id for ${searchterm}: ${error}`);
+			logger.error(
+				`[radarr-api]  Failed to lookup IDs for ${chalk.yellow(
+					searchterm
+				)} - (${chalk.red(
+					String(error).split(":").slice(1)[0].trim()
+				)})`
+			);
 			return resultOfErr(false);
 		}
 	} else {
