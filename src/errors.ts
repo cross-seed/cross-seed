3275269396d32cb2666a4ba667df6ec859ce9719
Michael Goodnow <mmgoodnow@gmail.com>
2024-05-23 01:52:35 -0400

    fix accidental reintroduction of stacks into CrossSeedErrors

--- src/errors.ts
+++ src/errors.ts
@@ -1,6 +1,10 @@
 import { logger } from "./logger.js";
 
 export class CrossSeedError extends Error {
+	constructor(message?: string, options?: ErrorOptions) {
+		super(message, options);
+		delete this.stack;
+	}
 	print(): void {
 		logger.error(this.message);
 		if (this.cause) {
