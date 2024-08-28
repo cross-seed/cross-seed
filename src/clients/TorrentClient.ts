c0b796be18e90433b4fbab52ac610100debc072b
ShanaryS <shanarys@gmail.com>
2024-08-28 18:32:40 -0400

    Prevent usage of cross-seed inject without a client

--- src/clients/TorrentClient.ts
+++ src/clients/TorrentClient.ts
@@ -8,7 +8,7 @@ import Transmission from "./Transmission.js";
 import Deluge from "./Deluge.js";
 import { Result } from "../Result.js";
 
-let activeClient: TorrentClient;
+let activeClient: TorrentClient | null = null;
 
 export interface TorrentClient {
 	isTorrentComplete: (
@@ -44,7 +44,7 @@ function instantiateDownloadClient() {
 	}
 }
 
-export function getClient(): TorrentClient {
+export function getClient(): TorrentClient | null {
 	if (!activeClient) {
 		instantiateDownloadClient();
 	}
