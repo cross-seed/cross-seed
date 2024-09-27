478b0048a27c45ae3e226d6b06da042c5f670130
ShanaryS <shanarys@gmail.com>
2024-09-27 01:41:51 -0400

    Readd skipRecheck since we can now resume torrents

--- src/clients/TorrentClient.ts
+++ src/clients/TorrentClient.ts
@@ -50,6 +50,7 @@ export interface TorrentClient {
 	}) => Promise<Map<string, string>>;
 	resumeInjection: (
 		infoHash: string,
+		decision: DecisionAnyMatch,
 		options: { checkOnce: boolean },
 	) => Promise<void>;
 	inject: (
@@ -87,8 +88,8 @@ export function shouldRecheck(
 	searchee: Searchee,
 	decision: DecisionAnyMatch,
 ): boolean {
-	const { matchMode } = getRuntimeConfig();
-	if (matchMode === MatchMode.SAFE) return true;
+	const { skipRecheck } = getRuntimeConfig();
+	if (!skipRecheck) return true;
 	if (decision === Decision.MATCH_PARTIAL) return true;
 	if (!searchee.infoHash) return true;
 	if (hasExt(searchee.files, VIDEO_DISC_EXTENSIONS)) return true;
@@ -96,8 +97,9 @@ export function shouldRecheck(
 }
 
 // Resuming partials
-export function getMaxRemainingBytes() {
+export function getMaxRemainingBytes(decision: DecisionAnyMatch) {
 	const { matchMode, maxRemainingForResume } = getRuntimeConfig();
+	if (decision !== Decision.MATCH_PARTIAL) return 0;
 	if (matchMode !== MatchMode.PARTIAL) return 0;
 	return maxRemainingForResume * 1024 * 1024;
 }
