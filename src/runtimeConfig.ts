a15819aa1801178c96f475fa081fc3322dcbc27c
ShanaryS <shanarys@gmail.com>
2024-11-26 01:19:25 -0500

    searchLimit per unique queries per indexer

--- src/runtimeConfig.ts
+++ src/runtimeConfig.ts
@@ -35,7 +35,7 @@ export interface RuntimeConfig {
 	rssCadence?: number;
 	snatchTimeout?: number;
 	searchTimeout?: number;
-	searchLimit: number;
+	searchLimit?: number;
 	blockList: string[];
 	apiKey?: string;
 	sonarr: string[];
