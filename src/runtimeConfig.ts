5b97420d876c347f49800d023e5e020e592241a9
ShanaryS <shanarys@gmail.com>
2024-12-11 14:42:01 -0500

    ensure duplicateCategory paths are valid on injection

--- src/runtimeConfig.ts
+++ src/runtimeConfig.ts
@@ -11,7 +11,7 @@ export interface RuntimeConfig {
 	linkType: LinkType;
 	flatLinking: boolean;
 	maxDataDepth: number;
-	linkCategory: string;
+	linkCategory?: string;
 	torrentDir?: string;
 	outputDir: string;
 	injectDir?: string;
