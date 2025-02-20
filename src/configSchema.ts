67c4647c97798a8ce8235a24241083b9fbe23744
ShanaryS <shanarys@gmail.com>
2025-02-20 11:18:00 -0500

    support null on injectDir

--- src/configSchema.ts
+++ src/configSchema.ts
@@ -369,7 +369,7 @@ export const VALIDATION_SCHEMA = z
 				}
 				return true;
 			}),
-		injectDir: z.string().optional(),
+		injectDir: z.string().nullish(),
 		includeSingleEpisodes: z.boolean(),
 		includeNonVideos: z.boolean(),
 		fuzzySizeThreshold: z
