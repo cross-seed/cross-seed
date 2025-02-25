b1d43b2f4c4ed774cb4136bf46a85014f75ce679
screenflickerr <accounts@screenflicker.com>
2025-02-24 17:38:05 -0800

    fix(config): update error message handling/display

--- packages/webui/src/features/ConfigForm/FieldInfo.tsx
+++ packages/webui/src/features/ConfigForm/FieldInfo.tsx
@@ -3,9 +3,8 @@ import type { FieldApi } from '@tanstack/react-form';
 export function FieldInfo({ field }: { field: FieldApi<any, any, any, any> }) {
   return (
     <>
-      {field.state.meta.errors.length ? (
-        // {field.state.meta.isTouched && field.state.meta.errors.length ? (
-        <span className="text-red-600">
+      {field.state.meta.isTouched && field.state.meta.errors.length ? (
+        <span className="text-sm text-red-600">
           {field.state.meta.errors.join(', ')}
         </span>
       ) : null}
