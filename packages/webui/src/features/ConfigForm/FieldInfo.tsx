dc6ab930bdcf1c6e1db8d67656426ee1c93a3492
screenflickerr <accounts@screenflicker.com>
2025-02-26 21:07:43 -0800

    fix(config): field level validation errors

--- packages/webui/src/features/ConfigForm/FieldInfo.tsx
+++ packages/webui/src/features/ConfigForm/FieldInfo.tsx
@@ -1,14 +1,28 @@
 import type { FieldApi } from '@tanstack/react-form';
 
-export function FieldInfo({ field }: { field: FieldApi<any, any, any, any> }) {
+export function FieldInfo({
+  field,
+  index,
+}: {
+  field: FieldApi<any, any, any, any>;
+  index?: number;
+}) {
+  const errors = field.state.meta.errors;
+
+  const getError = () => {
+    if (typeof index === 'undefined') {
+      return errors[0];
+    }
+
+    const errorArray =
+      typeof errors[0] === 'string' ? errors[0]?.split(',') : [];
+    return errorArray[index];
+  };
   return (
     <>
-      {field.state.meta.isTouched && field.state.meta.errors.length ? (
-        <span className="text-sm text-red-600">
-          {field.state.meta.errors.join(', ')}
-        </span>
+      {field.state.meta.isTouched && errors && errors.length ? (
+        <span className="text-sm text-red-600">{getError()}</span>
       ) : null}
-      {field.state.meta.isValidating ? 'Validating...' : null}
     </>
   );
 }
