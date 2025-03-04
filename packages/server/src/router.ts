be238bfbf3c2c7bf06567d43cae6e66c8575d961
screenflickerr <accounts@screenflicker.com>
2025-03-03 21:00:13 -0800

    fix(server): connect to cross-seed (on localhost)

--- packages/server/src/router.ts
+++ packages/server/src/router.ts
@@ -1,38 +1,101 @@
 import { promises as fs } from 'fs';
 import { router, publicProcedure } from './trpc';
 import path from 'path';
-// import { z } from 'zod';
+
+// Router for the server, must call the cross-seed API
+const host = 'localhost'; // '192.168.1.234';
+const port = '2468';
+
+const url = `http://${host}:${port}`;
 
 export const appRouter = router({
-  healthcheck: publicProcedure.query(() => {
-    return { status: 'ok', timestamp: new Date().toISOString() };
+  healthcheck: publicProcedure.query(async () => {
+    console.log('pinging cross-seed');
+    try {
+      const response = await fetch(`${url}/api/ping`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to ping cross-seed: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return {
+        status: 'fetch error',
+        message: error,
+        timestamp: new Date().toISOString(),
+      };
+    }
   }),
 
   /**
    * Config checking endpoints
    */
   config: router({
-    get: publicProcedure.query(() => {
-      return {};
+    get: publicProcedure.query(async () => {
+      // get config file contents from cross-seed
+      try {
+        const response = await fetch(`${url}/api/config`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(`Failed to read config file: ${response.statusText}`);
+        }
+
+        return await response.json();
+      } catch (error) {
+        throw new Error(`Failed to read config file: ${error}`);
+      }
+    }),
+
+    // save config file contents to cross-seed
+    save: publicProcedure.mutation(async ({ input }) => {
+      try {
+        const response = await fetch(`${url}/api/config`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+          body: JSON.stringify(input),
+        });
+
+        if (!response.ok) {
+          throw new Error(`Failed to save config file: ${response.statusText}`);
+        }
+
+        return await response.json();
+      } catch (error) {
+        throw new Error(`Failed to save config file: ${error}`);
+      }
     }),
 
     validate: publicProcedure.query(async () => {
       // validate config or get validation status
       try {
-        const response = await fetch(
-          'http://localhost:2468/api/config/validate',
-          {
-            headers: {
-              Accept: 'application/json',
-            },
+        const response = await fetch(`${url}/api/config/validate`, {
+          headers: {
+            Accept: 'application/json',
           },
-        );
+        });
 
         if (!response.ok) {
           throw new Error(`Failed to validate config: ${response.statusText}`);
         }
 
-        return response.json();
+        return await response.json();
       } catch (error) {
         throw new Error(`Failed to validate config: ${error}`);
       }
