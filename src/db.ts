c248442a3eb904bda99a6b93d279b9ff5c1eb7a9
Shanary <86130442+ShanaryS@users.noreply.github.com>
2025-05-10 19:13:37 -0400

    fix(job/cleanup): run tasks serially to reduce resource usage (#977)
    * Running cleanup tasks serially and in separate functions for resource
    management
    * Added logging for each task
    * Will not run the cleanup job until all other jobs are completed (runs
    immediately if triggered from api)

--- src/db.ts
+++ src/db.ts
@@ -6,7 +6,7 @@ import { join } from "path";
 import { getClients } from "./clients/TorrentClient.js";
 import { appDir } from "./configuration.js";
 import { TORRENT_CACHE_FOLDER } from "./constants.js";
-import { logger } from "./logger.js";
+import { Label, logger } from "./logger.js";
 import { getRuntimeConfig } from "./runtimeConfig.js";
 import { migrations } from "./migrations/migrations.js";
 import { cacheEnsembleTorrentEntry } from "./torrent.js";
@@ -27,64 +27,81 @@ export const db = knex({
 export async function cleanupDB(): Promise<void> {
 	const { dataDirs, seasonFromEpisodes, useClientTorrents } =
 		getRuntimeConfig();
-	await Promise.all([
-		(async () => {
-			if (!useClientTorrents) return;
-			const searchees = await flatMapAsync(
-				getClients(),
-				async (client) =>
-					(await client.getClientSearchees({ refresh: [] }))
-						.searchees,
-			);
-			if (!seasonFromEpisodes) return;
-			const ensembleRows = await flatMapAsync(
-				searchees,
-				async (searchee) =>
-					(await cacheEnsembleTorrentEntry(searchee)) ?? [],
-			);
-			await inBatches(ensembleRows, async (batch) => {
-				await db("ensemble")
-					.insert(batch)
-					.onConflict(["client_host", "path"])
-					.merge();
-			});
-		})(),
-		(async () => {
-			if (!dataDirs.length) return;
-			const deletedPaths = await filterAsync(
-				(await db("data").select("path")).map((e) => e.path),
-				(p) => notExists(p),
-			);
-			await inBatches(deletedPaths, async (batch) => {
-				await db("data").whereIn("path", batch).del();
-				await db("ensemble").whereIn("path", batch).del();
-			});
-		})(),
-		(async () => {
-			if (!seasonFromEpisodes) return;
-			const deletedPaths = await filterAsync(
-				(await db("ensemble").select("path")).map((e) => e.path),
-				(p) => notExists(p),
-			);
-			await inBatches(deletedPaths, async (batch) => {
-				await db("data").whereIn("path", batch).del();
-				await db("ensemble").whereIn("path", batch).del();
-			});
-		})(),
-		(async () => {
-			const torrentCacheDir = join(appDir(), TORRENT_CACHE_FOLDER);
-			const files = await readdir(torrentCacheDir);
-			const now = Date.now();
-			for (const file of files) {
-				const filePath = join(torrentCacheDir, file);
-				if (now - (await stat(filePath)).atimeMs > ms("1 year")) {
-					logger.verbose(`Deleting ${filePath}`);
-					await db("decision")
-						.where("info_hash", file.split(".")[0])
-						.del();
-					await unlink(filePath);
-				}
+	await (async () => {
+		if (!useClientTorrents) return;
+		logger.verbose({
+			label: Label.CLEANUP,
+			message: "Refreshing all client torrents",
+		});
+		const searchees = await flatMapAsync(
+			getClients(),
+			async (client) =>
+				(await client.getClientSearchees({ refresh: [] })).searchees,
+		);
+		if (!seasonFromEpisodes) return;
+		logger.verbose({
+			label: Label.CLEANUP,
+			message: "Refreshing all ensemble torrents",
+		});
+		const ensembleRows = await flatMapAsync(
+			searchees,
+			async (searchee) =>
+				(await cacheEnsembleTorrentEntry(searchee)) ?? [],
+		);
+		await inBatches(ensembleRows, async (batch) => {
+			await db("ensemble")
+				.insert(batch)
+				.onConflict(["client_host", "path"])
+				.merge();
+		});
+	})();
+	await (async () => {
+		if (!dataDirs.length) return;
+		logger.verbose({
+			label: Label.CLEANUP,
+			message: "Pruning deleted dataDirs entries",
+		});
+		const deletedPaths = await filterAsync(
+			(await db("data").select("path")).map((e) => e.path),
+			(p) => notExists(p),
+		);
+		await inBatches(deletedPaths, async (batch) => {
+			await db("data").whereIn("path", batch).del();
+			await db("ensemble").whereIn("path", batch).del();
+		});
+	})();
+	await (async () => {
+		if (!seasonFromEpisodes) return;
+		logger.verbose({
+			label: Label.CLEANUP,
+			message: "Pruning deleted ensemble entries",
+		});
+		const deletedPaths = await filterAsync(
+			(await db("ensemble").select("path")).map((e) => e.path),
+			(p) => notExists(p),
+		);
+		await inBatches(deletedPaths, async (batch) => {
+			await db("data").whereIn("path", batch).del();
+			await db("ensemble").whereIn("path", batch).del();
+		});
+	})();
+	await (async () => {
+		logger.verbose({
+			label: Label.CLEANUP,
+			message: "Pruning old torrent cache entries",
+		});
+		const torrentCacheDir = join(appDir(), TORRENT_CACHE_FOLDER);
+		const files = await readdir(torrentCacheDir);
+		const now = Date.now();
+		for (const file of files) {
+			const filePath = join(torrentCacheDir, file);
+			if (now - (await stat(filePath)).atimeMs > ms("1 year")) {
+				logger.verbose(`Deleting ${filePath}`);
+				await db("decision")
+					.where("info_hash", file.split(".")[0])
+					.del();
+				await unlink(filePath);
 			}
-		})(),
-	]);
+		}
+	})();
 }
