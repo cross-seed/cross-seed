db6bcb8660e1760148c83a7a38f9bbee663f78d0
screenflickerr <accounts@screenflicker.com>
2025-05-18 14:24:18 -0700

    fix(form): remove empty array values
    Empty array values (`[""]`) are properly updated to `[]` in
    `form.state.value` in the `onSubmit` function
    
    Clean up JSDoc in `transformers.ts`

--- packages/webui/src/features/ConfigForm/lib/transformers.ts
+++ packages/webui/src/features/ConfigForm/lib/transformers.ts
@@ -1,7 +1,7 @@
 /**
  * Remove any empty string values from an array of strings.
- * @param {string[]} array - The array of strings to filter.
- * @return {string[]} - The filtered array with empty string values removed.
+ * @param  array - The array of strings to filter.
+ * @return - The filtered array with empty string values removed.
  */
 export function removeEmptyArrayValues(array: string[]) {
   return array.filter((item) => item !== '');
