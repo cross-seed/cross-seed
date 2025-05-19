930e0facbf1b80cce8097f56ecd1311c6b97772b
screenflickerr <accounts@screenflicker.com>
2025-05-18 21:14:41 -0700

    refactor(ConfigForm): update react-form and refactor form
    Break out the gigantic single-page form into smaller, grouped chunks of
    fields utilizing updated tanstack/react-form conventions.
    
    Start adding some custom hooks for config/forms.

---
+++ packages/webui/src/hooks/form.tsx
@@ -0,0 +1,14 @@
+import { createFormHook } from '@tanstack/react-form';
+import { lazy } from 'react';
+import { fieldContext, formContext } from '../contexts/Form/form-context';
+
+export const { useAppForm, withForm } = createFormHook({
+  fieldContext,
+  formContext,
+  fieldComponents: {
+    TextField: lazy(() => import('../components/Form/text-field')),
+    SwitchField: lazy(() => import('../components/Form/switch-field')),
+    SelectField: lazy(() => import('../components/Form/select-field')),
+  },
+  formComponents: {},
+});
