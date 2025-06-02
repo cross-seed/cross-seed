1dd2c9fe36c046fe7aed6fc7b9b547ce6b6aa77b
Michael Goodnow <mmgoodnow@gmail.com>
2025-06-02 16:46:56 -0700

    fix styling

--- packages/webui/src/routes/__root.tsx
+++ packages/webui/src/routes/__root.tsx
@@ -30,9 +30,7 @@ export const Route = createRootRouteWithContext<RouterContext>()({
               <div className="flex flex-1 flex-col">
                 <div className="@container/main flex flex-1 flex-col gap-2">
                   <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                    <div className="px-4 lg:px-6">
-                      <Outlet />
-                    </div>
+                    <Outlet />
                   </div>
                 </div>
               </div>
