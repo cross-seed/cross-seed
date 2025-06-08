5b4f0dbcf0156c331d0a88b794dc3eef1d9b95ee
screenflickerr <accounts@screenflicker.com>
2025-06-08 15:56:20 -0700

    chore: break up validation schemas
    Separates validation schemas to match forms. Adds missing form fields
    and updates field organization a little.
    
    Cleans up some of the excess hooks/contexts.

--- packages/webui/src/hooks/use-config-form.tsx
+++ packages/webui/src/hooks/use-config-form.tsx
@@ -3,11 +3,13 @@ import { baseValidationSchema } from '@/types/config';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { formatConfigDataForForm } from '@/lib/formatConfigData';
 import { Config } from '@/types/config';
+import { ZodObject, ZodRawShape } from 'zod';
 // import { defaultConfig } from '../../../shared/constants';
 // import { formOptions } from '@tanstack/react-form';
 
-export const useConfigForm = () => {
+export const useConfigForm = (schema: ZodObject<ZodRawShape>) => {
   const trpc = useTRPC();
+
   const { data: config } = useSuspenseQuery(
     trpc.settings.get.queryOptions(undefined, {
       select: (data) => formatConfigDataForForm(data.config),
@@ -15,7 +17,11 @@ export const useConfigForm = () => {
   );
 
   const isFieldRequired = (field: string) => {
-    const schemaField = baseValidationSchema.shape[field as keyof Config];
+    const schemaField = schema.shape[field as keyof Config];
+    if (!schemaField) {
+      console.warn(`Field "${field}" not found in schema.`);
+      return false;
+    }
     return !schemaField.isOptional() && !schemaField.isNullable();
   };
 
