b774d25685efc458cd5228d5d8df89cc3c558de1
screenflickerr <accounts@screenflicker.com>
2025-06-07 20:39:29 -0700

    chore: rename config routes to settings
    Updates configRouter (and associated imports/uses) to settingsRouter

--- packages/webui/src/pages/Settings/connect.tsx
+++ packages/webui/src/pages/Settings/connect.tsx
@@ -25,7 +25,7 @@ const ConnectSettings = withForm({
       // isLoading,
       // isError,
     } = useQuery(
-      trpc.config.get.queryOptions(undefined, {
+      trpc.settings.get.queryOptions(undefined, {
         select: (data) => formatConfigDataForForm(data.config),
       }),
     );
