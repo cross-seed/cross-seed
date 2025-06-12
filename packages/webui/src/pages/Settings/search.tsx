4deefa96a3f662daeb30c224d3f37c6c7c4cca94
screenflickerr <accounts@screenflicker.com>
2025-06-12 00:37:40 -0700

    fix: update schema validation on forms
    Updates the forms to validate properly against their specific
    validations instead of the full validation schema.
    
    Updates the query on each form to only return the data in the schema for
    that form instead of the entire settings object.

--- packages/webui/src/pages/Settings/search.tsx
+++ packages/webui/src/pages/Settings/search.tsx
@@ -10,6 +10,7 @@ import { removeEmptyArrayValues } from '@/lib/transformers';
 import { searchValidationSchema } from '@/types/config';
 import useConfigForm from '@/hooks/use-config-form';
 import { FormValidationProvider } from '@/contexts/Form/form-validation-provider';
+import { pickSchemaFields } from '@/lib/pick-schema-fields';
 
 const SearchRssSettings = withForm({
   ...formOpts,
@@ -22,7 +23,16 @@ const SearchRssSettings = withForm({
       // isError,
     } = useQuery(
       trpc.settings.get.queryOptions(undefined, {
-        select: (data) => formatConfigDataForForm(data.config),
+        select: (data) => {
+          const fullDataset = formatConfigDataForForm(data.config);
+          const filteredData = pickSchemaFields(
+            searchValidationSchema,
+            fullDataset,
+            { includeUndefined: true },
+          );
+
+          return filteredData;
+        },
       }),
     );
 
@@ -71,121 +81,121 @@ const SearchRssSettings = withForm({
 
     return (
       <FormValidationProvider isFieldRequired={isFieldRequired}>
-      <form
-        className="form flex flex-col gap-4"
-        onSubmit={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-          form.handleSubmit();
-        }}
-        noValidate
-      >
-        {/* form fields */}
-        <div className="flex flex-wrap gap-6">
-          <fieldset className="form-fieldset border-border w-full gap-6 rounded-md border">
-            <legend>Searching and RSS</legend>
+        <form
+          className="form flex flex-col gap-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            form.handleSubmit();
+          }}
+          noValidate
+        >
+          {/* form fields */}
+          <div className="flex flex-wrap gap-6">
+            <fieldset className="form-fieldset border-border w-full gap-6 rounded-md border">
+              <legend>Searching and RSS</legend>
 
-            {/* TODO: Error states or validations don't seem to work for these fields */}
+              {/* TODO: Error states or validations don't seem to work for these fields */}
 
-            <div className="">
-              <form.AppField
-                name="delay"
-                validators={
-                  {
-                    // onBlur: baseValidationSchema.shape.delay,
+              <div className="">
+                <form.AppField
+                  name="delay"
+                  validators={
+                    {
+                      // onBlur: baseValidationSchema.shape.delay,
+                    }
                   }
-                }
-              >
-                {(field) => <field.TextField label="Delay" type="number" />}
-              </form.AppField>
-            </div>
-            <div className="">
-              <form.AppField name="matchMode">
-                {(field) => (
-                  <field.SelectField label="Match Mode" options={MatchMode} />
-                )}
-              </form.AppField>
-            </div>
-            <div className="">
-              <form.AppField
-                name="rssCadence"
-                validators={
-                  {
-                    // onBlur: baseValidationSchema.shape.rssCadence,
+                >
+                  {(field) => <field.TextField label="Delay" type="number" />}
+                </form.AppField>
+              </div>
+              <div className="">
+                <form.AppField name="matchMode">
+                  {(field) => (
+                    <field.SelectField label="Match Mode" options={MatchMode} />
+                  )}
+                </form.AppField>
+              </div>
+              <div className="">
+                <form.AppField
+                  name="rssCadence"
+                  validators={
+                    {
+                      // onBlur: baseValidationSchema.shape.rssCadence,
+                    }
                   }
-                }
-              >
-                {(field) => <field.TextField label="RSS Cadence" />}
-              </form.AppField>
-            </div>
-            <div className="">
-              <form.AppField
-                name="searchCadence"
-                validators={
-                  {
-                    // onBlur: baseValidationSchema.shape.searchCadence,
+                >
+                  {(field) => <field.TextField label="RSS Cadence" />}
+                </form.AppField>
+              </div>
+              <div className="">
+                <form.AppField
+                  name="searchCadence"
+                  validators={
+                    {
+                      // onBlur: baseValidationSchema.shape.searchCadence,
+                    }
                   }
-                }
-              >
-                {(field) => <field.TextField label="Search Cadence" />}
-              </form.AppField>
-            </div>
-            <div className="">
-              <form.AppField
-                name="searchTimeout"
-                validators={
-                  {
-                    // onBlur: baseValidationSchema.shape.searchTimeout,
+                >
+                  {(field) => <field.TextField label="Search Cadence" />}
+                </form.AppField>
+              </div>
+              <div className="">
+                <form.AppField
+                  name="searchTimeout"
+                  validators={
+                    {
+                      // onBlur: baseValidationSchema.shape.searchTimeout,
+                    }
                   }
-                }
-              >
-                {(field) => <field.TextField label="Search Timeout" />}
-              </form.AppField>
-            </div>
-            <div className="">
-              <form.AppField
-                name="searchLimit"
-                validators={
-                  {
-                    // onBlur: baseValidationSchema.shape.searchLimit,
+                >
+                  {(field) => <field.TextField label="Search Timeout" />}
+                </form.AppField>
+              </div>
+              <div className="">
+                <form.AppField
+                  name="searchLimit"
+                  validators={
+                    {
+                      // onBlur: baseValidationSchema.shape.searchLimit,
+                    }
                   }
-                }
-              >
-                {(field) => (
-                  <field.TextField label="Search Limit" type="number" />
-                )}
-              </form.AppField>
-            </div>
-            <div className="">
-              <form.AppField
-                name="excludeOlder"
-                validators={
-                  {
-                    // onBlur: baseValidationSchema.shape.excludeOlder,
+                >
+                  {(field) => (
+                    <field.TextField label="Search Limit" type="number" />
+                  )}
+                </form.AppField>
+              </div>
+              <div className="">
+                <form.AppField
+                  name="excludeOlder"
+                  validators={
+                    {
+                      // onBlur: baseValidationSchema.shape.excludeOlder,
+                    }
                   }
-                }
-              >
-                {(field) => <field.TextField label="Exclude Older" />}
-              </form.AppField>
-            </div>
-            <div className="">
-              <form.AppField
-                name="excludeRecentSearch"
-                validators={
-                  {
-                    // onBlur: baseValidationSchema.shape.excludeRecentSearch,
+                >
+                  {(field) => <field.TextField label="Exclude Older" />}
+                </form.AppField>
+              </div>
+              <div className="">
+                <form.AppField
+                  name="excludeRecentSearch"
+                  validators={
+                    {
+                      // onBlur: baseValidationSchema.shape.excludeRecentSearch,
+                    }
                   }
-                }
-              >
-                {(field) => <field.TextField label="Exclude Recent Search" />}
-              </form.AppField>
-            </div>
-          </fieldset>
-          <form.AppForm>
-            <form.SubmitButton />
-          </form.AppForm>
-        </div>
-      </form>
+                >
+                  {(field) => <field.TextField label="Exclude Recent Search" />}
+                </form.AppField>
+              </div>
+            </fieldset>
+            <form.AppForm>
+              <form.SubmitButton />
+            </form.AppForm>
+          </div>
+        </form>
       </FormValidationProvider>
     );
   },
