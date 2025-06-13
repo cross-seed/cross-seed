9e8a5355cc06366725e11c9d4404ed88321558fc
Michael Goodnow <mmgoodnow@gmail.com>
2025-06-12 21:47:18 -0700

    rename indexers to trackers

--- packages/webui/src/routes/settings/indexers.tsx
+++
@@ -1,6 +0,0 @@
-import IndexerSettings from '@/pages/Settings/indexers';
-import { createFileRoute } from '@tanstack/react-router';
-
-export const Route = createFileRoute('/settings/indexers')({
-  component: IndexerSettings,
-});
