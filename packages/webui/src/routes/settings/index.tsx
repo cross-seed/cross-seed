b971ba004b6d08f5870badad3b51bcb5cfaf2db5
Michael Goodnow <mmgoodnow@gmail.com>
2025-06-13 16:32:50 -0700

    missed a file

--- packages/webui/src/routes/settings/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-import GeneralSettings from '@/pages/Settings/general';
-
-export const Route = createFileRoute('/settings/')({
-  component: GeneralSettings,
-});
