506efc0185ea34babb81be0da7024f574f957895
Michael Goodnow <mmgoodnow@gmail.com>
2025-08-30 14:42:59 -0400

    Squashed commit of the following:
    commit ef302428e2a798773b181427b0cd0e8ac6f08575
    Author: Michael Goodnow <mmgoodnow@gmail.com>
    Date:   Sat Aug 30 14:41:45 2025 -0400
    
        add enabled flag
    
    commit 08032c9b24003afd216a690e8ed849e1f27628fa
    Author: Michael Goodnow <mmgoodnow@gmail.com>
    Date:   Sat Aug 30 14:13:09 2025 -0400
    
        remove unused migration

---
+++ src/migrations/14-indexer-enabled-flag.ts
@@ -0,0 +1,19 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.alterTable("indexer", (table) => {
+		table.boolean("enabled");
+	});
+	await knex("indexer").update({ enabled: true });
+	await knex.schema.alterTable("indexer", (table) => {
+		table.boolean("enabled").notNullable().alter();
+	});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	await knex.schema.alterTable("indexer", (table) => {
+		table.dropColumn("enabled");
+	});
+}
+
+export default { name: "14-indexer-enabled-flag", up, down };
