ca64daff1040f1249874638979540d1145e8c2ef
Michael Goodnow <mmgoodnow@gmail.com>
2026-03-19 20:35:18 -0700

    chore: upgrade vite and backend tooling

--- packages/webui/eslint.config.js
+++ packages/webui/eslint.config.js
@@ -19,6 +19,9 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'react-hooks/immutability': 'off',
+      'react-hooks/purity': 'off',
+      'react-hooks/set-state-in-effect': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
